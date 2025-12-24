94cbf9b0471a467cffcc74127a3d857a7125449c
CyanSalt <c.salt@hotmail.com>
2025-12-24 16:38:46 +0800

    refactor: use box in badge

--- src/badge/index.vue
+++ src/badge/index.vue
@@ -1,10 +1,7 @@
 <script lang="ts" setup>
 import '../common/style.scss'
-import type { RoughSVG } from 'roughjs/bin/svg'
-import { getLengthProperty, useTransitionListener } from '../common/property'
-import RGraphics from '../graphics/index.vue'
+import RBox from '../box/index.vue'
 import type { GraphicsProps } from '../graphics/utils'
-import { getSVGSize } from '../graphics/utils'
 
 defineOptions({
   name: 'RBadge',
@@ -21,34 +18,13 @@ defineSlots<{
   /** Decorated content of the badge. */
   default?: (props: {}) => any,
 }>()
-
-const { timestamp, listener } = useTransitionListener('::before')
-
-function draw(rc: RoughSVG, svg: SVGSVGElement) {
-  void timestamp.value
-  const { width, height } = getSVGSize(svg)
-  const strokeWidth = getLengthProperty(svg, '--R-badge-border-width') ?? 0
-  const padding = 2
-  const ellipse = rc.ellipse(
-    width / 2,
-    height / 2,
-    width - padding * 2,
-    height - padding * 2,
-    {
-      strokeWidth,
-      stroke: 'var(--R-badge-color)',
-    },
-  )
-  svg.appendChild(ellipse)
-}
 </script>
 
 <template>
-  <span class="r-badge" @transitionrun="listener">
-    <sup class="r-badge__marker">
-      <RGraphics :options="graphicsOptions" @draw="draw" />
+  <span class="r-badge">
+    <RBox tag="sup" round :graphics-options="graphicsOptions" class="r-badge__marker">
       <span class="r-badge__marker-content"><slot name="marker"></slot></span>
-    </sup>
+    </RBox>
     <slot></slot>
   </span>
 </template>
@@ -89,13 +65,10 @@ function draw(rc: RoughSVG, svg: SVGSVGElement) {
   --R-badge-padding-inline: var(--r-badge-padding-inline, calc((1em - 4px) - 4px));
   // Width of the badge marker border.
   --R-badge-border-width: var(--r-badge-border-width, var(--r-common-stroke-width));
+  // Box styles
+  --r-box-border-color: var(--R-badge-color);
+  --r-box-border-width: var(--R-badge-border-width);
   position: relative;
-  &::before {
-    border-top-style: solid;
-    @include partials.transition-runner((
-      --R-badge-border-width: border-top-width,
-    ));
-  }
 }
 .r-badge__marker {
   --r-element-font-size: calc(1em - 4px);
