2197639298deb0973ed8acdc744795716de46e02
CyanSalt <c.salt@hotmail.com>
2025-12-24 17:12:10 +0800

    refactor: use box in toast

--- src/toast/index.vue
+++ src/toast/index.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
 import '../common/style.scss'
-import type { RoughSVG } from 'roughjs/bin/svg'
-import { useTemplateRef, watchEffect } from 'vue'
-import { getLengthProperty, useTransitionListener } from '../common/property'
-import type { ColorProps, SizeProps } from '../common/utils'
+import { useCurrentElement } from '@vueuse/core'
+import { computed, watchEffect } from 'vue'
+import RBox from '../box/index.vue'
 import { useLocal } from '../common/utils'
-import RGraphics from '../graphics/index.vue'
 import type { GraphicsProps } from '../graphics/utils'
-import { getFilledSizeOptions, getSVGSize } from '../graphics/utils'
 
 defineOptions({
   name: 'RToast',
@@ -16,8 +13,6 @@ defineOptions({
 const {
   duration = 3000,
   open = true,
-  color,
-  size,
   graphicsOptions,
 } = defineProps<{
   /**
@@ -30,7 +25,7 @@ const {
    * @default true
    */
   open?: boolean,
-} & ColorProps & SizeProps & GraphicsProps>()
+} & GraphicsProps>()
 
 const emit = defineEmits<{
   /** Callback function triggered when visibility of the toast is changed. */
@@ -64,12 +59,12 @@ watchEffect(onInvalidate => {
   }
 })
 
-const root = useTemplateRef<HTMLElement>('root')
+const root = useCurrentElement<HTMLElement | null>()
 
 watchEffect(() => {
   if (!root.value) return
   try {
-    if (internalOpen) {
+    if (internalOpen.value) {
       root.value.showPopover()
     } else {
       root.value.hidePopover()
@@ -81,34 +76,25 @@ watchEffect(() => {
   }
 })
 
-const { timestamp, listener } = useTransitionListener('::before')
-
-function draw(rc: RoughSVG, svg: SVGSVGElement) {
-  void timestamp.value
-  const { width, height } = getSVGSize(svg)
-  const strokeWidth = getLengthProperty(svg, '--R-toast-border-width') ?? 0
-  const padding = 2
-  const rectangle = rc.rectangle(padding, padding, width - padding * 2, height - padding * 2, {
-    strokeWidth,
-    stroke: 'var(--R-toast-color)',
+const nestingGraphicsOptions = computed(() => {
+  return {
     fill: 'var(--r-common-background-color)',
     fillStyle: 'solid',
-    ...getFilledSizeOptions(strokeWidth),
-  })
-  svg.appendChild(rectangle)
-}
+    ...graphicsOptions,
+  }
+})
 </script>
 
 <template>
-  <div
+  <RBox
+    tag="div"
+    :graphics-options="nestingGraphicsOptions"
     popover="manual"
-    :class="['r-toast', color, size]"
+    class="r-toast"
     @toggle="toggle"
-    @transitionrun="listener"
   >
-    <RGraphics :options="graphicsOptions" @draw="draw" />
     <slot></slot>
-  </div>
+  </RBox>
 </template>
 
 <style lang="scss">
@@ -121,12 +107,6 @@ function draw(rc: RoughSVG, svg: SVGSVGElement) {
   initial-value: currentColor;
 }
 
-@property --R-toast-border-width {
-  syntax: '<length>';
-  inherits: true;
-  initial-value: 0px;
-}
-
 @layer roughness__base {
   .r-toast {
     @include reset.popover;
@@ -135,8 +115,8 @@ function draw(rc: RoughSVG, svg: SVGSVGElement) {
 .r-toast {
   // Color of the toast text and border.
   --R-toast-color: var(--r-toast-color, var(--r-element-color));
-  // Width of the toast border.
-  --R-toast-border-width: var(--r-toast-border-width, var(--r-common-stroke-width));
+  // Box styles
+  --r-box-border-color: var(--R-toast-color);
   position: fixed;
   inset: unset;
   inset-block-start: 75%;
@@ -145,13 +125,5 @@ function draw(rc: RoughSVG, svg: SVGSVGElement) {
   padding-inline: var(--r-common-box-padding-inline);
   color: var(--R-toast-color);
   transform: translate(-50%, -50%);
-  @include partials.colored-element();
-  @include partials.sized-element();
-  &::before {
-    border-top-style: solid;
-    @include partials.transition-runner((
-      --R-toast-border-width: border-top-width,
-    ));
-  }
 }
 </style>
