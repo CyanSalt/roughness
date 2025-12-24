6967fdbc3243c2c36106233ba03a65bd8d845018
CyanSalt <c.salt@hotmail.com>
2025-12-24 17:20:46 +0800

    refactor: use box in upload

--- src/upload/index.vue
+++ src/upload/index.vue
@@ -1,14 +1,10 @@
 <script lang="ts" setup>
 import '../common/style.scss'
-import type { RoughSVG } from 'roughjs/bin/svg'
 import type { InputHTMLAttributes } from 'vue'
 import { computed } from 'vue'
-import { getLengthProperty, getLengthPropertyAsArray, useTransitionListener } from '../common/property'
+import RBox from '../box/index.vue'
 import { isTruthyBooleanish } from '../common/utils'
 import { useName } from '../form/utils'
-import RGraphics from '../graphics/index.vue'
-import type { GraphicsProps } from '../graphics/utils'
-import { getFilledSizeOptions, getSVGSize } from '../graphics/utils'
 import RLoading from '../loading/index.vue'
 
 defineOptions({
@@ -33,23 +29,19 @@ interface InputProps {
 const {
   block = false,
   disabled: userDisabled,
-  filled = false,
   loading = false,
   multiple = false,
   name: userName,
-  graphicsOptions,
   ...props
 } = defineProps<{
   /** Whether the upload is displayed as block. */
   block?: boolean,
-  /** Whether the upload is filled with its color. */
-  filled?: boolean,
   /**
    * Whether the upload is loading.
    * It will be non-interactive in loading state.
    */
   loading?: boolean,
-} & InputProps & GraphicsProps>()
+} & InputProps>()
 
 const emit = defineEmits<{
   /** Callback function triggered when one or more files are selected. */
@@ -67,31 +59,6 @@ const disabled = computed(() => {
   return isTruthyBooleanish(userDisabled) || loading
 })
 
-const { timestamp, listener } = useTransitionListener('::before')
-
-function draw(rc: RoughSVG, svg: SVGSVGElement) {
-  void timestamp.value
-  const { width, height } = getSVGSize(svg)
-  const strokeWidth = getLengthProperty(svg, '--R-upload-border-width') ?? 0
-  const strokeLineDash = getLengthPropertyAsArray(svg, '--R-upload-border-dash')
-    ?.map(value => value ?? 0) ?? undefined
-  const padding = 2
-  const rectangle = rc.rectangle(
-    padding,
-    padding,
-    width - padding * 2,
-    height - padding * 2,
-    {
-      stroke: 'var(--R-upload-border-color)',
-      strokeWidth,
-      strokeLineDash,
-      fill: filled ? 'var(--R-upload-color)' : undefined,
-      ...getFilledSizeOptions(strokeWidth),
-    },
-  )
-  svg.appendChild(rectangle)
-}
-
 function change(event: InputEvent) {
   const target = event.target as HTMLInputElement
   const files = target.files!
@@ -103,9 +70,9 @@ function change(event: InputEvent) {
 </script>
 
 <template>
-  <label
+  <RBox
+    tag="label"
     :class="['r-upload', { 'is-block': block }]"
-    @transitionrun="listener"
   >
     <input
       :disabled="disabled"
@@ -116,10 +83,9 @@ function change(event: InputEvent) {
       class="r-upload__input"
       @change="change"
     >
-    <RGraphics :options="graphicsOptions" @draw="draw" />
     <slot></slot>
     <RLoading v-if="loading" class="r-upload__loading" />
-  </label>
+  </RBox>
 </template>
 
 <style lang="scss">
@@ -160,19 +126,16 @@ function change(event: InputEvent) {
   // An odd number of values will be repeated to yield an even number of values. Thus, `8` is equivalent to `8 8`.
   // See [`stroke-dasharray`](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dasharray).
   --R-upload-border-dash: var(--r-upload-border-dash, var(--r-common-stroke-dash));
+  // Box styles
+  --r-box-border-color: var(--R-upload-border-color);
+  --r-box-border-width: var(--R-upload-border-width);
+  --r-box-border-dash: var(--R-upload-border-dash);
   display: inline-block;
   padding-block: var(--r-common-box-padding-block);
   padding-inline: var(--r-common-box-padding-inline);
   color: var(--R-upload-color);
   text-align: center;
   cursor: pointer;
-  &::before {
-    border-top-style: solid;
-    @include partials.transition-runner((
-      --R-upload-border-width: border-top-width,
-      --R-upload-border-dash: border-spacing,
-    ));
-  }
   &:focus-visible, &:active {
     // @default 2px when focused or active
     --R-upload-border-width: var(--r-upload-border-width, var(--r-common-emphasized-stroke-width));
@@ -182,8 +145,8 @@ function change(event: InputEvent) {
     cursor: not-allowed;
     text-decoration-line: line-through;
   }
-  &.is-filled:not(:disabled) {
-    text-shadow: var(--r-common-stroke-text-shadow);
+  &.is-filled:disabled {
+    text-shadow: none;
   }
   &.is-block {
     display: block;
